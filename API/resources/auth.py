24fd6302dd8654f27ed2be0369b0eaab84e6fdfb
MaliciousMonk45456 <maliciousmonk45456@gmail.com>
2024-01-12 09:44:26 +0530

    Added API side decorators

--- API/resources/auth.py
+++ API/resources/auth.py
@@ -8,6 +8,8 @@ import datetime
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import re
+from functools import wraps
 
 DATABASE=os.getenv("DATABASE")
 USER=os.getenv("USER")
@@ -15,7 +17,20 @@ PASSWORD=os.getenv("PASSWORD")
 HOST=os.getenv("HOST")
 PORT=os.getenv("PORT")
 
+def validator(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        body = request.get_json()
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+        pat = re.compile(reg)
+        if(re.fullmatch(regex, body['username']) and re.search(pat, body['password'])):
+            return f(*args, **kwargs)
+        return {"message":"Please enter valid email and password"},400
+    return decorated
+
 class RegisterApi(Resource):
+    @validator
     def post(self):
         body = request.json
         conn = psycopg2.connect(database=DATABASE, user=USER, 
