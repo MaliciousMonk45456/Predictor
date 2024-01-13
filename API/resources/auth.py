e7371af03fc8ea5d27e1b913980b4c073ccc1839
MaliciousMonk45456 <maliciousmonk45456@gmail.com>
2024-01-13 13:40:43 +0530

    Added virtual environment

--- API/resources/auth.py
+++ API/resources/auth.py
@@ -2,7 +2,6 @@ from flask import request
 from flask_restful import Resource
 import psycopg2
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask import request
 from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 import datetime
 import os
