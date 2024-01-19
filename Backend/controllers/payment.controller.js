98aee57d0b3d4a9a27931518dcb2411733b24105
MaliciousMonk45456 <maliciousmonk45456@gmail.com>
2024-01-19 18:25:16 +0530

    Minor changes

--- Backend/controllers/payment.controller.js
+++ Backend/controllers/payment.controller.js
@@ -122,12 +122,12 @@ const verifyorder = async (req, res, next) => {
         //         }
         //     });
         // });
-        const resend = new Resend("re_4wYp4LVa_FnZ7qh62LX7FRgz7W5ueH6zv");
+        const resend = new Resend(process.env.RESEND_API_KEY);
 
         resend.emails.send({
-          from: "onboarding@resend.dev",
-          to: "tripathiaryan361@gmail.com",
-          subject: "Hello World",
+          from: "recommendationsystem00@gmail.com",
+          to: payment.email,
+          subject: "Payment Receipt",
           html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
         });
       } catch (err) {
